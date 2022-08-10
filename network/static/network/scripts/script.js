d04393a38cb6d0e8c055f954a3f70fbd2700da4e
IFIOK UDOH <fiokudoh01@gmail.com>
2022-08-10 23:16:39 +0100

    9th August 2022 36. add javascript implementation the edit button to show the edit text area, cancel and save buttons
    10th August 2022
    37. add javascript implementation for the cancel button
    38. add javascript implementation for the save button

--- network/static/network/scripts/script.js
+++ network/static/network/scripts/script.js
@@ -46,29 +46,97 @@ document.addEventListener('DOMContentLoaded', function() {
                     console.log(`The exception is ${err.message}`)
                 })
             
-        });
+        })
     })
+
+
+    if( document.querySelector(".edit_toggle") != null ){
+            document.querySelector(".edit_toggle").onclick = (evt) => {
+            
+            target_edit_btn = evt.target
+            target_post_div = target_edit_btn.parentElement.nextElementSibling
+
+            post_id = target_edit_btn.dataset.postid
+            post_content = target_post_div.innerText
+
+            console.log(`The post id to edit is ${post_id}`)
+            console.log(`The post content to edit is ${post_content}`)
+
+            sessionStorage.setItem("post_id", post_id)
+            sessionStorage.setItem("post_content", post_content)
+
+            post_edit_div = document.createElement("div")
+
+            textarea_div = document.createElement("div")
+            textarea = document.createElement("textarea")
+            textarea.setAttribute("id", "txtarea_post_edit")
+            textarea.innerHTML = post_content
+            textarea.dataset.postid = post_id
+
+            console.log(textarea)
+
+            textarea_div.append(textarea)
+
+            buttons_div = document.createElement("div")
+
+            cancel_btn = document.createElement("button")
+            cancel_btn.innerHTML = "Cancel"
+            cancel_btn.setAttribute("id", "cancel_post_edit")
+            cancel_btn.onclick = (evt) => {
+
+                post_id = sessionStorage.getItem("post_id")
+                post_content = sessionStorage.getItem("post_content")
+    
+                console.log(`The post id to edit is ${post_id}`)
+                console.log(`The post content to edit is ${post_content}`)
+    
+                post_content_div = document.createElement("div")
+                post_content_div.setAttribute("id", "post_content")
+                post_content_div.innerHTML = post_content
+    
+                
+                target_post_div.replaceChildren()
+                target_post_div.append(post_content_div)
+                
+            }
+
+            save_btn = document.createElement("button")
+            save_btn.innerHTML = "Save"
+            save_btn.setAttribute("id", "save_post_edit")
+            save_btn.onclick = (evt) => {
+                //Get the post content from the browser's session storage and display
+    
+                console.log("The save button was clicked.")
+                post_content_edit = document.querySelector("#post_content_edit")
+    
+                fetch('/post/edit/' + post_id,{
+                    method: 'PUT',
+                    body: JSON.stringify({
+                    'post_id': post_id
+                    })
+                })
+                .then( response.json() )
+                .then(result => {
+                    
+                })
+                
+            }
+
+            buttons_div.append(cancel_btn)
+            buttons_div.append(save_btn)
+
+            post_edit_div.append(textarea_div)
+            post_edit_div.append(buttons_div)
+
+            target_post_div.replaceChildren()
+            target_post_div.append(post_edit_div)
+            console.log("edit text area shoould show now")
+            console.log(target_post_div)
+
+        }
+
+    }
+  
     
 
-    document.querySelector("#follow_toggle").addEventListener("click", (evt) => {
-        console.log("The follow button was clicked.")
-    })
-
-
-    document.querySelector("#edit_toggle").addEventListener("click", (evt) => {
-        console.log("The follow button was clicked.")
-        post_content_edit = document.querySelector("#post_content_edit")
-
-        fetch('/post/edit/' + post_id,{
-            method: 'PUT',
-            body: JSON.stringify({
-              'post_id': post_id
-            })
-        })
-        .then( response.json() )
-        .then(result => {
-            
-        })
-    })
-
-});
+})
